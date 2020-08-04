23aeab75a806bcaad57c17c1fcdee1b53301b343
christinathihoang <christinahoang@utexas.edu>
2020-08-04 01:23:13 -0500

    add menu component

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Menu from "./components/Menu";
+import { Wrapper } from "./styledComponents";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <div className="columns">
+      <div className="column is-one-quarter">
+        <Menu />
+      </div>
+      <div className="column has-background-grey-dark"></div>
+
     </div>
   );
 }
---
+++ src/components/NicknameForm.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import 'bulma/css/bulma.css'
+
+
+function NicknameForm() {
+  return (
+    <div></div>
+  )
+}
+
+export default NicknameForm;
