7a3792a6e387a662478e97e6cc4deb2b4fae98b6
christinathihoang <christinahoang@utexas.edu>
2020-08-05 00:20:04 -0500

    create contact

---
+++ src/routes/Contact.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import "bulma/css/bulma.css";
+
+function Contact() {
+  return (
+    <div className="box">
+      <div className="tabs">
+        <ul>
+          <li className="is-active">
+            <a>Contest Nickname Status</a>
+          </li>
+          <li>
+            <a>National Board</a>
+          </li>
+          <li>
+            <a>Site Issues</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default Contact;
