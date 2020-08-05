aa5fc82e345cc30d51260291aa2cef47bef2f106
christinathihoang <christinahoang@utexas.edu>
2020-08-04 23:15:02 -0500

    implement page styling with padding

---
+++ src/components/Footer.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "bulma/css/bulma.css";
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <div className="content has-text-centered">
+        <p>
+          <strong>Cerberus</strong> by <a href="https://github.com/christinathihoang/cerberus">Christina Hoang</a>.
+        </p>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
--- src/routes/Profile.tsx
+++ src/routes/Profile.tsx
@@ -2,11 +2,41 @@ import React from "react";
 import "bulma/css/bulma.css";
 
 function Profile() {
-    return (
-      <div className="box">
-        
+  return (
+    <div className="box">
+            <article className="message">
+        <div className="message-body">
+          Before applying for a nickname, please make sure that all of the
+          information on this page is correct.
+        </div>
+      </article>
+
+
+      <div className="card">
+        <div className="card-content">
+          <p className="title">
+           Christina Hoang
+          </p>
+          <p className="subtitle">*Bishop*</p>
+        </div>
+        <footer className="card-footer">
+          <p className="card-footer-item">
+            <span>
+              View on{" "}
+              <a href="https://twitter.com/codinghorror/status/506010907021828096">
+                Twitter
+              </a>
+            </span>
+          </p>
+          <p className="card-footer-item">
+            <span>
+              Share on <a href="#">Facebook</a>
+            </span>
+          </p>
+        </footer>
       </div>
-    )
+    </div>
+  );
 }
 
 export default Profile;
